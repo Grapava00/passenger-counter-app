ee3388b8b6a553e2f6baebbf60127ebe2c7ec3be
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-19 02:52:38 +0400

    add function js for app

---
+++ app.js
@@ -0,0 +1,19 @@
+//=>უნდა დავიჭიროთ ის ელემენტები, რომლებზეც გვინდა მანიპულირება
+let title=document.querySelector("span");
+let paragraph=document.querySelector("p");
+
+//=>უნდა დავწეროთ ფუნქციები ღილაკებისთვის
+
+let count=0;
+
+function incrementNum(){
+    count++;
+    title.textContent=count;
+}
+
+function saveNum(){
+    let save=count+" - "
+    paragraph.textContent+=save;
+    count=0;
+}
+
