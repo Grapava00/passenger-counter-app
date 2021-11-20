90ecfa98103ad0b90215c4f1ec10d43100c531ca
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-20 16:49:26 +0400

    update comments

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-//=>უნდა დავიჭიროთ ის ელემენტები, რომლებზეც გვინდა მანიპულირება
-let title=document.querySelector("span");
-let paragraph=document.querySelector("p");
+//=>access element
+let title = document.querySelector("span");
+let paragraph = document.querySelector("p");
 
-//=>უნდა დავწეროთ ფუნქციები ღილაკებისთვის
+//=>functions for buttons
 
-let count=0;
+let count = 0;
 
-function incrementNum(){
-   count++;
-    title.textContent=count;
+function incrementNum() {
+    count++;
+    title.textContent = count;
 }
 
-function decrementNum(){
+function decrementNum() {
     count--;
-    title.textContent=count;
+    title.textContent = count;
 }
 
-function saveNum(){
-    let save=count+" - ";
-    paragraph.textContent+=save;
+function saveNum() {
+    let save = count + " - ";
+    paragraph.textContent += save;
 }
-function clearNum(){
-    count=0;
-    title.textContent=count;
+function clearNum() {
+    count = 0;
+    title.textContent = count;
 }
