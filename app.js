e249f5ca2cd8da95365fcffa5e539be46c393e0b
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2022-01-06 21:07:49 +0400

    add addEventListener() to the buttons

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-//=>access element
+//access elements
+
+const incrementBtn=document.getElementById("increment-btn");
+const decrementBtn=document.getElementById("decrement-btn");
+const saveBtn=document.getElementById("save-btn");
+const clearBtn=document.getElementById("clear-btn");
 let title = document.querySelector("span");
 let paragraph = document.querySelector("p");
-
-//=>functions for buttons
-
 let count = 0;
 
-function incrementNum() {
+
+//functions for buttons
+
+incrementBtn.addEventListener("click", function(){
     count++;
     title.textContent = count;
-}
+})
 
-function decrementNum() {
+decrementBtn.addEventListener("click", function(){
     count--;
     title.textContent = count;
-}
+})
 
-function saveNum() {
-    let save = count + " - ";
-    paragraph.textContent += save;
-}
-function clearNum() {
+saveBtn.addEventListener("click", function(){
+    let saveNum=count+ " - ";
+    paragraph.textContent+=saveNum;
+})
+
+clearBtn.addEventListener("click", function(){
     count = 0;
     title.textContent = count;
-}
+})
